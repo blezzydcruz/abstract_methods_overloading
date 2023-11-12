6df2c197d3f189997690e038383046c106b986fb
blezzydcruz <mbannedelacruz@hotmail.com>
2023-11-12 17:05:54 +0000

    updated

---
+++ src/main/java/Lion.java
@@ -0,0 +1,15 @@
+public class Lion extends Animal{
+
+    private boolean canWaitToBeKing;
+
+    public Lion(String name, boolean canWaitToBeKing){
+        super(name);
+        this.canWaitToBeKing = canWaitToBeKing;
+    }
+
+//    @Override
+    public String makeNoise(){
+        return "ROAR!";
+    }
+
+}
---
+++ src/test/java/LionTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LionTest {
+
+    Lion lion;
+
+    @BeforeEach
+    void setUp(){
+        lion = new Lion("Simba", false);
+    }
+
+    @Test
+    void canMakeNoise(){
+        assertThat(lion.makeNoise()).isEqualTo("ROAR!");
+    }
+
+
+    @Test
+    void canEat(){
+        assertThat(lion.eat()).isEqualTo("Mmm, that was tasty!");
+    }
+
+}
